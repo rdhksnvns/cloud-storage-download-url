1837c8fcf5fba0609aa8e589e275f941b2184ef5
Vansh Gupta <rdhksnvns@gmail.com>
2020-07-27 18:11:42 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const projectId = 'ecell-5b937'
+const keyFilename = 'key.json'
+
+const bucketName = 'ecell-5b937.appspot.com';
+const srcFilename = 'certificate_template.jpg';
+var downloadUrl;
+
+// Imports the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage({projectId, keyFilename});
+
+async function url() {
+
+  // Get the download url of the file
+  downloadUrl = await storage.bucket(bucketName).file(srcFilename).getSignedUrl({
+    action: 'read',
+    expires: '03-09-2491'
+  });
+
+  console.log(`Download url: ` + downloadUrl);
+}
+
+url().catch(console.error);
